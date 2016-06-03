0c2a214fe1c9ea36bb6b10c59872a4baacf7b3c0
Niu1234 <635449644@qq.com>
2016-06-03 19:24:39 +0800

    11

--- LittleMylyn/src/littlemylyn/controllers/DBAction.java
+++ LittleMylyn/src/littlemylyn/controllers/DBAction.java
@@ -2,12 +2,19 @@ package littlemylyn.controllers;
 
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 import littlemylyn.entities.Task;
 import littlemylyn.model.NodeWrapper;
@@ -42,9 +49,6 @@ public class DBAction extends Action  {
 		}
 	}
 	
-
-	
-	
 	public boolean isClass(Object node){
 		return (node instanceof NodeWrapper) && (((NodeWrapper) node).getParent() instanceof List);
 	}
@@ -65,16 +69,40 @@ public class DBAction extends Action  {
 			public void run(){
 				LittleMylynView.getViewer().setInput(TaskManager.getManager());
 			}
-		});
+		});	
 		
 	}
 	
-	
-	
 	//open sepcific file in the eclipse editor
 	private void openInEditor(NodeWrapper<String> node){
-		System.out.println("________________________");
-		System.out.println(node.toString());
+		//System.out.println("________________________");
+		//System.out.println(node.toString());
+		String temp1[]=node.toString().split("\\.");
+		String fName="";
+		String rfName=temp1[1];
+		String temp2[]=temp1[0].split("/");
+		String lfName="";
+		for(int i=1;i<temp2.length;i++){
+			lfName+="/"+temp2[i];
+		}
+		String projectname=temp2[0];
+		fName=lfName+"/"+rfName+".java";
+		fName=fName.replaceAll("\\s*", "");
 		// TODO This is for Niu1234.
-	}
+		if (fName != null) {
+			//System.out.println(fName);
+			IWorkbenchPage wbPage = PlatformUI.getWorkbench()  
+			        .getActiveWorkbenchWindow().getActivePage();
+			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectname);
+			IFile file = project.getFile(fName);  
+			try {  
+			    if (file != null) {  
+			    IDE.openEditor(wbPage, file);  
+			    }  
+			 } catch (PartInitException e) {  
+			 e.printStackTrace();  
+			 }  
+		return;  
+	  } 
+	} 
 }
